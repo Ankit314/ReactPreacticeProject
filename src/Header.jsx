d00d13ac9f43de0f4f565da809e899936f0ac472
ankit <ankitkumar@gmail.com>
2025-01-28 21:42:57 +0530

    second app

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// export default function Header() {
+//   return (
+//     <div>Header</div>
+//   )
+// }
+
+
+import React from 'react'
+
+export default function Header(emails){
+    console.log(emails.email)
+    return(
+        <div>Header</div>
+        
+    )
+    
+}
